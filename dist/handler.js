90c26316de199e513eac81f195924713a716a72f
Carlos Mocholi <carlossmocholi@gmail.com>
2021-11-14 23:32:09 +0100

    Remove source-map-support

--- dist/handler.js
+++ dist/handler.js
@@ -4,8 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var serverless_lambda_1 = require("@probot/serverless-lambda");
-var source_map_support_1 = require("source-map-support");
 var _1 = __importDefault(require("./"));
-// Needed for traceback translation from transpiled javascript -> typescript
-source_map_support_1.install();
 module.exports.probot = serverless_lambda_1.serverless(_1.default);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import {install} from 'source-map-support';
-
 import autoCcBot from './auto-cc-bot';
 // import autoLabelBot from './auto-label-bot';
 import {Application} from 'probot';
-
-// Needed for traceback translation from transpiled javascript -> typescript
-install();
 
 function runBot(app: Application): void {
   autoCcBot(app);
