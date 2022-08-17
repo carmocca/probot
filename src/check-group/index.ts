9467959f98b5f0c98a305f241c54f06c9751dce0
Carlos Mocholí <carlossmocholi@gmail.com>
2022-08-17 18:59:07 +0200

    Adapt code to run

--- src/check-group/index.ts
+++ src/check-group/index.ts
@@ -2,45 +2,18 @@
  * The entrypoint for the Probot app.
  * @module CheckGroupApp
  */
-import * as Sentry from '@sentry/node';
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {Context, Probot} from 'probot';
 /* eslint-enable @typescript-eslint/no-unused-vars */
 import {checkRunEventHandler, pullRequestEventHandler} from './handlers';
-import dotenv from 'dotenv';
-import prettyjson from 'prettyjson';
 
-// TODO(@tianhaoz95): consolidate this into a location called
-// third_party to indicate that we are not in control of whether
-// this works or not.
-// For setup details, see:
-// https://docs.sentry.io/platforms/node
-dotenv.config();
-Sentry.init({
-  dsn: process.env['SENTRY_DSN'],
-  tracesSampleRate: 1.0
-});
-
-/**
- * Top level Probot app.
- */
-const checkGroupApp = (app: Probot): void => {
+function checkGroupApp(app: Probot): void {
   app.on('pull_request', async (context: Context<'pull_request'>) => {
-    try {
-      await pullRequestEventHandler(context);
-    } catch (err) {
-      context.log.error(prettyjson.render(err));
-      Sentry.captureException(err);
-    }
+    await pullRequestEventHandler(context);
   });
 
   app.on('check_run', async (context: Context<'check_run'>) => {
-    try {
-      await checkRunEventHandler(context);
-    } catch (err) {
-      context.log.error(prettyjson.render(err));
-      Sentry.captureException(err);
-    }
+    await checkRunEventHandler(context);
   });
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import autoCcBot from './auto-cc/auto-cc-bot';
+import checkGroupApp from './check-group/index';
 import {Probot} from 'probot';
 
 export default function botSteps(app: Probot): void {
   autoCcBot(app);
+  checkGroupApp(app);
 }
